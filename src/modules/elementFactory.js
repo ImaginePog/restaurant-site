01f8418f3f0d1c61a5fdc7d8458b4f9eff224cd1
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-04-21 20:14:45 +0545

    Create element factory module

---
+++ src/modules/elementFactory.js
@@ -0,0 +1,16 @@
+//Creates element with given parameters
+export default function elementFactory(elementName, ...attributes) {
+	const genericElement = document.createElement(elementName);
+
+	attributes.forEach((attribute) => {
+		if (attribute.name === "text") {
+			genericElement.innerText = attribute.value;
+		} else if (attribute.name === "class") {
+			genericElement.classList.add(attribute.value);
+		} else {
+			genericElement.setAttribute(attribute.name, attribute.value);
+		}
+	});
+
+	return genericElement;
+}
