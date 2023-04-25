2a5fc42356ab571061fd90eb931fec83002cd06f
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-04-25 19:44:03 +0545

    Organize webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,16 @@ module.exports = {
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: "asset/resource",
+				generator: {
+					filename: "./images/[name].[contenthash].[ext]",
+				},
 			},
 			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
 				type: "asset/resource",
+				generator: {
+					filename: "./fonts/[name].[ext]",
+				},
 			},
 		],
 	},
